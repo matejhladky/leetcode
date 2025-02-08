cf13dfb026f560b52dcede74ed23dfe85b4c810c
mathladky <mat.hladky@gmail.com>
2025-02-08 09:47:00 +0100

    one-liner solution baby

--- 1071-greatest-common-divisor-of-strings/cpp/greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/cpp/greatest-common-divisor-of-strings.cpp
@@ -46,3 +46,8 @@ string gcdOfStrings(string str1, string str2) {
     int gcdLength = gcd(str1.size(), str2.size());
     return str1.substr(0, gcdLength);
 }
+
+// one-liner
+string gcdOfStrings(string str1, string str2) {
+    return (str1 + str2 == str2 + str1) ? str1.substr(0, gcd(size(str1),size(str2))): "";
+}
